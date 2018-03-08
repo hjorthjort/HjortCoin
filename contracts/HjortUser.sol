78c0c534d842b1888f8cc0b4d3b8bfedd354c6e2
hjorthjort <rikard.hjort@gmail.com>
2018-03-08 17:15:55 +0900

    Add user accounts

---
+++ contracts/HjortUser.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.17;
+
+import "./HjortCoin.sol";
+
+contract HjortUser {
+
+    address public hjortCoinAddress;
+
+    struct User {
+        bytes32 username;
+        address account;
+    }
+
+    User[] public users;
+
+    function HjortUser(address _hjortCoinAddress) {
+        hjortCoinAddress = _hjortCoinAddress;
+    }
+
+    function register(bytes32 _username) public returns (bool success) {
+        for (uint256 i; i < users.length; i++) {
+            if (users[i].username == _username) {
+                return false;
+            }
+        }
+        User memory user = User(_username, msg.sender);
+        users.push(user);
+        return true;
+    }
+
+    function balanceOf(bytes32 _username) public view returns (uint256 balance) {
+        HjortCoin hc = HjortCoin(hjortCoinAddress);
+        for (uint256 i; i < users.length; i++) {
+            if (users[i].username == _username) {
+                return hc.balanceOf(users[i].account);
+            }
+        }
+        return 0;
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var HjortCoin = artifacts.require("HjortCoin");
+var HjortUser = artifacts.require("HjortUser");
 
 module.exports = function(deployer) {
     deployer.deploy(HjortCoin, "HjortCoin", "HJC", 18, 9001);
+    deployer.deploy(HjortUser, HjortCoin.address);
 }
